5651da57247f3236c48166d28eadf05b2ba421f6
Marcin Bielak <marcin.bieli@gmail.com>
2023-08-10 00:49:45 +0200

    remove duplicate in test code

--- tests/codecs/test_iot_ext_codec.py
+++ tests/codecs/test_iot_ext_codec.py
@@ -9,13 +9,8 @@ MSG_1_EXAMPLE = '''t|3900237526042,d|device_name_001,m|val_water_001=i:1234,m|va
                 '''m|connector_state=b:0,m|temp_01=d:34.4,m|temp_02=d:36.4,m|temp_03=d:10.4,m|pwr=d:12.231,''' \
                 '''m|current=d:1.429,m|current_battery=d:1.548'''
 
-
-class IoTextCodecTest(TestCase):
-
-    # TODO: verify where is a bug!?
-    @skip
-    def test_decode(self):
-        expected = [Item(kind=ItemTypes.TIMESTAMP_MILIS, name='3900237526042', metric=None),
+MSG_1_EXAMPLE_AS_DATA_STRUCTS = \
+    [Item(kind=ItemTypes.TIMESTAMP_MILIS, name='3900237526042', metric=None),
                     Item(kind=ItemTypes.DEVICE_ID, name='device_name_001', metric=None),
                     Item(kind=ItemTypes.METRIC_ITEM, name='val_water_001', metric=MetricDataItem(
                         data_type=MetricDataTypes.INTEGER, value=1234)),
@@ -38,35 +33,22 @@ class IoTextCodecTest(TestCase):
                     Item(kind=ItemTypes.METRIC_ITEM, name='current_battery', metric=MetricDataItem(
                         data_type=MetricDataTypes.DECIMAL, value=Decimal('1.548')))]
 
+class IoTextCodecTest(TestCase):
+
+    # TODO: verify where is a bug!?
+    @skip
+    def test_decode(self):
+        expected = MSG_1_EXAMPLE_AS_DATA_STRUCTS
+
         result = IoTextCodec.decode(MSG_1_EXAMPLE)
 
         self.assertEqual(expected, result)
 
     def test_encode(self):
         expected = MSG_1_EXAMPLE
-        iotext_msg = [Item(kind=ItemTypes.TIMESTAMP_MILIS, name='3900237526042', metric=None),
-                      Item(kind=ItemTypes.DEVICE_ID, name='device_name_001', metric=None),
-                      Item(kind=ItemTypes.METRIC_ITEM, name='val_water_001', metric=MetricDataItem(
-                          data_type=MetricDataTypes.INTEGER, value=1234)),
-                      Item(kind=ItemTypes.METRIC_ITEM, name='val_water_002', metric=MetricDataItem(
-                          data_type=MetricDataTypes.INTEGER, value=15)),
-                      Item(kind=ItemTypes.METRIC_ITEM, name='bulb_state', metric=MetricDataItem(
-                          data_type=MetricDataTypes.BOOL, value=True)),
-                      Item(kind=ItemTypes.METRIC_ITEM, name='connector_state', metric=MetricDataItem(
-                          data_type=MetricDataTypes.BOOL, value=False)),
-                      Item(kind=ItemTypes.METRIC_ITEM, name='temp_01', metric=MetricDataItem(
-                          data_type=MetricDataTypes.DECIMAL, value=Decimal('34.4'))),
-                      Item(kind=ItemTypes.METRIC_ITEM, name='temp_02', metric=MetricDataItem(
-                          data_type=MetricDataTypes.DECIMAL, value=Decimal('36.4'))),
-                      Item(kind=ItemTypes.METRIC_ITEM, name='temp_03', metric=MetricDataItem(
-                          data_type=MetricDataTypes.DECIMAL, value=Decimal('10.4'))),
-                      Item(kind=ItemTypes.METRIC_ITEM, name='pwr', metric=MetricDataItem(
-                          data_type=MetricDataTypes.DECIMAL, value=Decimal('12.231'))),
-                      Item(kind=ItemTypes.METRIC_ITEM, name='current', metric=MetricDataItem(
-                          data_type=MetricDataTypes.DECIMAL, value=Decimal('1.429'))),
-                      Item(kind=ItemTypes.METRIC_ITEM, name='current_battery', metric=MetricDataItem(
-                          data_type=MetricDataTypes.DECIMAL, value=Decimal('1.548')))]
+        iotext_msg = MSG_1_EXAMPLE_AS_DATA_STRUCTS
 
         result = IoTextCodec.encode(iotext_msg)
 
         self.assertEqual(expected, result)
+ 
